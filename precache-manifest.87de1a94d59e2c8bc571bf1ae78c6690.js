190c5ada3501dfaf21bd7677490395cf5d6774c6
flynn <lognamu@gmail.com>
2019-01-24 16:19:43 +0900

    Updates

---
+++ precache-manifest.87de1a94d59e2c8bc571bf1ae78c6690.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1b4c617c3d86cf9db113",
+    "url": "/todo-list/static/css/main.15494b7e.chunk.css"
+  },
+  {
+    "revision": "1b4c617c3d86cf9db113",
+    "url": "/todo-list/static/js/main.1b4c617c.chunk.js"
+  },
+  {
+    "revision": "a8978f017dc37c52266f",
+    "url": "/todo-list/static/js/1.a8978f01.chunk.js"
+  },
+  {
+    "revision": "6f47f260b12c7ad9f72e",
+    "url": "/todo-list/static/js/runtime~main.6f47f260.js"
+  },
+  {
+    "revision": "a6fe415c9a93d278143b3427972d5ab1",
+    "url": "/todo-list/index.html"
+  }
+];
